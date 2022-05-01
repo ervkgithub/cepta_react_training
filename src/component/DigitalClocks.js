048c06d912aad6662f3011649b48579d2dd58900
Vijay Kumar <vijay.kumar@altudo.co>
2022-05-02 01:57:48 +0530

    var name changes

--- src/component/DigitalClocks.js
+++ src/component/DigitalClocks.js
@@ -3,18 +3,18 @@ import React, { useState } from "react";
 
 const DigitalClock = () => {
     let time = new Date().toLocaleString();
-    const [cur_time, setCtime] = useState(time);
+    const [curTime, setCurTime] = useState(time);
 
     const UpdatedTime = () => {
         let time = new Date().toLocaleString();
-        setCtime(time);
+        setCurTime(time);
     }
     setInterval(UpdatedTime,1000);
     return (
         <>
             <div className="digital-clock-wrap">
                 <div className="dc-label">Digital Clock Time: </div>
-                <div className="date-time">{cur_time}</div>
+                <div className="date-time">{curTime}</div>
             </div>
         </>
     )
