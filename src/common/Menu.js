1225d444faea2a78dfb10b22d2e928ba626e2d95
Vijay Kumar <vijay.kumar@altudo.co>
2022-05-13 01:41:41 +0530

    Currency Converter Assignment worked

--- src/common/Menu.js
+++ src/common/Menu.js
@@ -7,26 +7,26 @@ const Menu = () => {
     return (
         <>
             <div className='bg-info'>
-                    <Navbar expand="lg">
-                        <Nav.Link><Link to="/home" className='btn btn-outline-success'>Home</Link></Nav.Link>
-                        <Nav.Link><Link to="/counter" className='btn btn-outline-success'>Counter</Link></Nav.Link>
-                        <Nav.Link><Link to="/todo" className='btn btn-outline-success'>ToDo List</Link></Nav.Link>
-                        <Nav.Link><Link to="/compmodal" className='btn btn-outline-success'>Modal Component</Link></Nav.Link>
-                        <Nav.Link><Link to="/compapi" className='btn btn-outline-success'>Fetch API 1</Link></Nav.Link>
-                        <Nav.Link><Link to="/fetchapi" className='btn btn-outline-success'>Fetch API 2</Link></Nav.Link>
-                        <Nav.Link><Link to="/accordemo" className='btn btn-outline-success'>Accordion Demo</Link></Nav.Link>
-                        <Nav.Link><Link to="/funtodolist" className='btn btn-outline-success'>Functional ToDOLIst</Link></Nav.Link>
-                        <Nav.Link><Link to="/digiclock" className='btn btn-outline-success'>Digital Clock Functional</Link></Nav.Link>
-                        <Nav.Link><Link to="/digiclockclass" className='btn btn-outline-success'>Digital Clock Class</Link></Nav.Link>
-                        <Nav.Link><Link to="/formfn" className='btn btn-outline-success'>Form Fn</Link></Nav.Link>
-                        <Nav.Link><Link to="/emicalc" className='btn btn-outline-success'>EMI Calculator Class</Link></Nav.Link>
-                        <Nav.Link><Link to="/contextdemo" className='btn btn-outline-success'>Context DEmo</Link></Nav.Link>
-                        <Nav.Link><Link to="/expensetracker" className='btn btn-outline-success'>Expense Tracker</Link></Nav.Link>
-                        <Nav.Link><Link to="/currencyconverter" className='btn btn-outline-success'>Currency Converter</Link></Nav.Link>
-                        <Nav.Link><Link to="/hoc1" className='btn btn-outline-success'>HOC1</Link></Nav.Link>
-                        <Nav.Link><Link to="/hoc2" className='btn btn-outline-success'>HOC2</Link></Nav.Link>
-                        
-                    </Navbar>
+                <Navbar expand="lg">
+                    <Nav.Link><Link to="/home" className='btn btn-outline-success'>Home</Link></Nav.Link>
+                    <Nav.Link><Link to="/counter" className='btn btn-outline-success'>Counter</Link></Nav.Link>
+                    <Nav.Link><Link to="/todo" className='btn btn-outline-success'>ToDo List</Link></Nav.Link>
+                    <Nav.Link><Link to="/compmodal" className='btn btn-outline-success'>Modal Component</Link></Nav.Link>
+                    <Nav.Link><Link to="/compapi" className='btn btn-outline-success'>Fetch API 1</Link></Nav.Link>
+                    <Nav.Link><Link to="/fetchapi" className='btn btn-outline-success'>Fetch API 2</Link></Nav.Link>
+                    <Nav.Link><Link to="/accordemo" className='btn btn-outline-success'>Accordion Demo</Link></Nav.Link>
+                    <Nav.Link><Link to="/funtodolist" className='btn btn-outline-success'>Functional ToDOLIst</Link></Nav.Link>
+                    <Nav.Link><Link to="/digiclock" className='btn btn-outline-success'>Digital Clock Functional</Link></Nav.Link>
+                    <Nav.Link><Link to="/digiclockclass" className='btn btn-outline-success'>Digital Clock Class</Link></Nav.Link>
+                    <Nav.Link><Link to="/formfn" className='btn btn-outline-success'>Form Fn</Link></Nav.Link>
+                    <Nav.Link><Link to="/emicalc" className='btn btn-outline-success'>EMI Calculator Class</Link></Nav.Link>
+                    <Nav.Link><Link to="/contextdemo" className='btn btn-outline-success'>Context DEmo</Link></Nav.Link>
+                    <Nav.Link><Link to="/expensetracker" className='btn btn-outline-success'>Expense Tracker</Link></Nav.Link>
+                    <Nav.Link><Link to="/currencyconverter" className='btn btn-outline-success'>Currency Converter</Link></Nav.Link>
+                    <Nav.Link><Link to="/hoc1" className='btn btn-outline-success'>HOC1</Link></Nav.Link>
+                    <Nav.Link><Link to="/hoc2" className='btn btn-outline-success'>HOC2</Link></Nav.Link>
+
+                </Navbar>
             </div>
         </>
     )
--- src/component/CurrencyConverter/CurrencyConverter.js
+++ src/component/CurrencyConverter/CurrencyConverter.js
@@ -1,66 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CurrencyConverter = () => {
 
-  let fromArr = ["USD", "INR"];
-  const toArr = ["INR", "USD"];
+  let fromArr = ["USD", "EUR", "COP"];
+  let toArr = ["CAD", "BRL", "BTN"];
 
-  var currAmount;
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [currAmount, setCurrAmount] = useState(0);
+  const [currFrom, setCurrFrom] = useState();
+  const [currTo, setCurrTo] = useState();
+
 
   const handleCurrencyAmount = (e) => {
-    console.log("handleCurrencyAmount");
-     currAmount = e.target.value;
+    console.log("handleCurrencyAmount", e.target.value);
+    let currAmountF = e.target.value;
+    setCurrAmount(currAmountF);
   }
 
   const handleCurrencyFrom = (e) => {
-    console.log("handleCurrencyFrom");
+    console.log("handleCurrencyFrom", e.target.value);
     console.log(e.target.value);
+    let currFromF = e.target.value;
+    setCurrFrom(currFromF);
   }
 
-  const handleCurrencyTO = () => {
-    console.log("handleCurrencyTo");
+  const handleCurrencyTO = (e) => {
+    console.log("handleCurrencyTO", e.target.value);
+    console.log(e.target.value);
+    let currToF = e.target.value;
+    setCurrTo(currToF);
   }
 
   const convertCurrency = () => {
-    console.log("convertCurrency");
-    var fromCurrency = fromArr.map((item) => (
-      fromCurrency = item
-    ));
-    var toCurrency = toArr.map((item) => (
-      toCurrency = item
-    ));
-    console.log(fromCurrency)
-    console.log(toCurrency)
-    for(var i = 0; i < fromCurrency.length; i++){
-      for(var j = 0; j < toCurrency.length; j++)
-      if(fromCurrency[i] === "INR" && toCurrency[j] === "USD"){
-        console.log("INR to USD", currAmount * 0.013);
-      }
-      if(fromCurrency[i] === "USD" && toCurrency[j] === "INR"){
-        console.log("INR to USD", currAmount * 77.30);
-      }
+    console.log("convertCurrencyFrom", currFrom);
+    console.log("convertCurrencyTo", currTo);
+    if (currFrom === "EUR" && currTo === "BRL") {
+      console.log("1 EUR to CAD", currAmount * 1.35);
+      const convertedAmountF = currAmount * 1.35;
+      setConvertedAmount(convertedAmountF);
     }
-  
+    else if (currFrom === "EUR" && currTo === "BTN") {
+      const convertedAmountF = currAmount * 80.33
+      console.log("1 EUR to BTN", currAmount * 80.33);
+      setConvertedAmount(convertedAmountF);
+    }
+    else if (currFrom === "COP" && currTo === "BRL") {
+      const convertedAmountF = currAmount * 0.0013
+      console.log("1 COP to BRL", currAmount * 0.0013);
+      setConvertedAmount(convertedAmountF);
+    }
+    else if (currFrom === "USD" && currTo === "CAD") {
+      const convertedAmountF = currAmount * 1.30
+      console.log("1 COP to BRL", currAmount * 1.30);
+      setConvertedAmount(convertedAmountF);
+    }
+    else {
+      alert("not mached currency");
+    }
+
   }
 
-  
   return (
     <>
-      <div className='curr-converter-wrap col-sm-12'>
+      <div className='curr-converter-wrap col-sm-6'>
         <h3 className='text-center'>Currency Converter</h3>
         <div className='curr-converter'>
           <div className='curr-input-wrap'>
             <label>Amount to convert</label>
-            <input type="number" onChange={handleCurrencyAmount} />
+            <input type="number" placeholder="Enter amount" onChange={handleCurrencyAmount} />
           </div>
           <div className='curr-input-wrap'>
             <label>From</label>
             <select onChange={handleCurrencyFrom}>
-            {
+              {
                 fromArr && fromArr.map((item) => (
-                  <option value={item}>{item}</option>
+                  <option>{item}</option>
                 ))
-            }
+              }
             </select>
           </div>
           {/* <div>
@@ -69,17 +85,17 @@ const CurrencyConverter = () => {
           <div className='curr-input-wrap'>
             <label>To</label>
             <select onChange={handleCurrencyTO}>
-            {
+              {
                 toArr && toArr.map((item) => (
                   <option>{item}</option>
                 ))
-            }
+              }
             </select>
           </div>
           <button type='button' className='btn btn-success text-center' onClick={convertCurrency}>Convert Currency</button>
           <div className='curr-input-wrap'>
             <label>Result</label>
-            <input type="text" />
+            <input type="text" value={convertedAmount} />
           </div>
         </div>
       </div>
