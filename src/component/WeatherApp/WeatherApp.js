c5607cf2bfede6ca5f5f6a98e4622ee9adfdb665
Vijay Kumar <vijay.kumar@altudo.co>
2022-05-28 02:09:50 +0530

    minor cosmetic changes

--- src/component/WeatherApp/WeatherApp.js
+++ src/component/WeatherApp/WeatherApp.js
@@ -5,20 +5,24 @@ import axios from 'axios';
 
 const WeatherApp = () => {
     const [weatherdata, setWeatherData] = useState(null);
-    const[searchcity, setSearchCity] = useState("London");
-    // const[lattitude, setLattitude] = useState(51.52);
-    // const[longitude, setLongitude] = useState(0.12);
+    const [searchcity, setSearchCity] = useState("London");
+    const [lattitude, setLattitude] = useState(51.52);
+    const [longitude, setLongitude] = useState(0.12);
 
-    const handleSearch = (e) =>{
-        setSearchCity(e.target.value)
-        console.log(searchcity);
+    const handleSearch = (e) => {
+        setSearchCity(e.target.value);
+        console.log("searchcity", searchcity);
+        setLattitude(lattitude => weatherdata.data.location.lat);
+        setLongitude(longitude => weatherdata.data.location.lon);
+        console.log("setLattitude", lattitude);
+        console.log("setLongitude", longitude);
     }
-   
+
     useEffect(() => {
         const options = {
             method: 'GET',
             url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
-            params: { q: searchcity, days: '3', lat: 51.52, lon: 0.12 },
+            params: { q: searchcity, days: '1', lattitude, lon: longitude },
             headers: {
                 'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
                 'X-RapidAPI-Key': '5a1042ca4cmsh594219d84e8fabap111938jsn2079b1264b60'
@@ -33,7 +37,7 @@ const WeatherApp = () => {
                 console.error(error);
             });
 
-    },[searchcity]);
+    }, [searchcity, lattitude, longitude]);
 
     return (
         <>
@@ -41,12 +45,12 @@ const WeatherApp = () => {
                 <h2>Simple Weather App</h2>
                 <div className='row'>
                     <div className="weather-search-wrap">
-                        <input type="text" id="search" onChange={handleSearch} placeholder="Search for City" />
-                        <button className="btn btn-danger" type="button" onChange={handleSearch}>Submit</button>
+                        <input type="text" id="search" onChange={handleSearch} placeholder="Search City" />
+                        {/* <button className="btn btn-danger" type="button" onChange={handleSearch}>Submit</button> */}
                     </div>
                 </div>
                 <div className='row' style={{ justifyContent: 'center' }}>
-                    <div className='col-md-6'>
+                    <div className='col-md-10'>
                         {weatherdata && <WeatherCards searchcity={searchcity} weatheralldata={weatherdata} />}
                     </div>
                 </div>
--- src/component/WeatherApp/WeatherCards.js
+++ src/component/WeatherApp/WeatherCards.js
@@ -2,11 +2,11 @@
 import React from "react";
 
 const WeatherCards = (props) => {
-    return(
+    return (
         <>
             <div className="weather-cards">
                 {
-                props.searchcity ? <h3>{props.searchcity}</h3> : <h3>London</h3>
+                    props.searchcity ? <h3>{props.searchcity}</h3> : <h3>London</h3>
                 }
                 <div className="degree">{props.weatheralldata.data.current.temp_c}<sup>0 C</sup></div>
                 <img src={props.weatheralldata.data.current.condition.icon} alt="icon" />
